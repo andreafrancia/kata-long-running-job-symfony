e98b211b9b95725a547180dfdb393426888da1fa
Andrea Francia <andreafrancia@users.noreply.github.com>
2022-05-26 19:50:56 +0200

    Refactor: remove unused method: JobRepository::remove()

--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -29,15 +29,6 @@ class JobRepository extends ServiceEntityRepository
         $this->getEntityManager()->flush();
     }
 
-    public function remove(Job $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
     /** @return string[] */
     public function findAllIds(): array
     {
