77c6fb62eed15226f8edb9f0ce98a1029b541e38
Andrea Francia <andreafrancia@users.noreply.github.com>
2022-05-28 19:54:07 +0200

    Remove unused import

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\JobStatus;
 use App\Repository\JobRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
