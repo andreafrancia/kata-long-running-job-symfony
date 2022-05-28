0ded2c8a9251c1e37576dda8b7871f29913319df
Andrea Francia <andreafrancia@users.noreply.github.com>
2022-05-28 19:30:45 +0200

    JobController::addNewJob uses a real uuid when is not tested.

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -6,15 +6,22 @@ use App\Repository\JobRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
 
 class JobController extends AbstractController
 {
     private ?string $jobIdForTest = null;
 
+    /*
+     * To test manually use:
+     *
+     * curl -X POST https://localhost:8002/job/add-new
+     *
+     */
     #[Route('/job/add-new', name: 'app_job', methods: ['POST'])]
     public function addNewJob(JobRepository $repository): JsonResponse
     {
-        $jobId = $this->jobIdForTest;
+        $jobId = $this->jobIdForTest ?? Uuid::v4();;
 
         $repository->addNewJob($jobId);
 
